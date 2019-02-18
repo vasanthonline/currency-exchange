f3c5e1b8df38c7baf4ad3a81cd1ccebc554e41d2
Vasanth <vasanth@indix.com>
2019-02-18 14:07:23 +0530

    Move to npx, so that node_modules can be used.

--- server/bin/setup.sh
+++ server/bin/setup.sh
@@ -3,19 +3,20 @@
 # Uncomment below 2 lines if nvm is not installed.
 # curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 source ~/.nvm/nvm.sh
-nvm install 10.15
-nvm use 10.15
+nvm install 10.15.1
+nvm use 10.15.1
 npm install yarn -g
+npm install npx -g
 
 # Client
 cd ./../client/
 yarn install --production=false
-CI=true npm run test
-npm run build
+CI=true npx run test
+npx run build
 
 # Server
 cd ./../server/
 yarn install --production=false
-npm run compile
-NODE_ENV=staging npm run test
+npx run compile
+NODE_ENV=staging npx run test
 
